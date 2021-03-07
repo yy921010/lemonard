5c7d9a26f084407b22b519ad07d37763ac72d3a8
charles <charlesmaxwellyoung@gmail.com>
2021-03-08 00:05:26 +0800

    chore: debug

--- src/components/UIKit/Dialog/index.tsx
+++ src/components/UIKit/Dialog/index.tsx
@@ -4,9 +4,7 @@ import 'twin.macro';
 import Icon from '../RemixIcon';
 import './index.scss';
 
-export type Dialog = DialogProps;
-
-function Dialog({ children, ...rest }: Dialog): JSX.Element {
+function Dialog({ children, ...rest }: DialogProps): JSX.Element {
     return (
         <RcDialog {...rest} closeIcon={<Icon name="close" tw="text-gray-100 text-2xl" />}>
             {children}
