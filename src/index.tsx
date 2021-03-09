ddab957ac2fdc21795bbb81e03c3b27456d976b4
charles <charlesmaxwellyoung@gmail.com>
2021-03-09 23:54:03 +0800

    feat: 新增grapgql功能

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,34 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from '@/components';
 import { UseRequestProvider } from 'ahooks';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
 import 'remixicon/fonts/remixicon.css';
 import './index.css';
 
+const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_URL,
+    cache: new InMemoryCache(),
+    connectToDevTools: (process.env.REACT_APP_IS_CONNECT_DEV_TOOL as unknown) as boolean,
+});
+
 ReactDOM.render(
     <BrowserRouter>
         <GlobalStyles />
-        <UseRequestProvider
-            value={{
-                refreshOnWindowFocus: false,
-                onError(error: Error) {
-                    console.log(error);
-                },
-            }}
-        >
-            <App />
-        </UseRequestProvider>
+        <ApolloProvider client={client}>
+            <UseRequestProvider
+                value={{
+                    refreshOnWindowFocus: false,
+                    onError(error: Error) {
+                        console.log(error);
+                    },
+                }}
+            >
+                <App />
+            </UseRequestProvider>
+        </ApolloProvider>
     </BrowserRouter>,
     document.getElementById('root'),
 );
